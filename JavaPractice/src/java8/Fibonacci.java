80576789e725ceab487d3a1230bf61d18fb92dd3
onlysnehal <32597444+onlysnehal@users.noreply.github.com>
2019-04-06 10:52:55 +0530

    Java 8 - code practice
    Java 8 - code practice

---
+++ JavaPractice/src/java8/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        //Generate Fibonacci series
+        System.out.println("Fibonacci series : ");
+        List<Integer> fibonnaciSeries =
+                Stream.iterate(new int[]{0, 1}, s -> new int[]{s[1], s[0] + s[1]})
+                .limit(10)
+                .map(n -> n[0])
+                .collect(Collectors.toList());
+
+        System.out.println(fibonnaciSeries);
+    }
+}
---
+++ JavaPractice/src/java8/FindMinMaxElement.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class FindMinMaxElement {
+
+    public static void main(String[] args) {
+
+        int [] array = {4,1,2,7,23,8,12};
+        System.out.println("Find min/max element from an array : "+Arrays.toString(array) );
+        //Using Java 8 - min/max element from an array
+        OptionalInt max = IntStream.of(array)
+                            .max();
+        System.out.println("Max element : "+max.getAsInt());
+
+        System.out.println("Min element : "+IntStream.of(array)
+                .min().getAsInt());
+
+        System.out.println("Using summary statistics : ");
+        System.out.println("Min : "+IntStream.of(array).summaryStatistics().getMin());
+        System.out.println("Max : "+IntStream.of(array).summaryStatistics().getMax());
+        System.out.println("Sum : "+IntStream.of(array).summaryStatistics().getSum());
+        System.out.println("Average : "+IntStream.of(array).summaryStatistics().getAverage());
+        System.out.println("Count : "+IntStream.of(array).summaryStatistics().getCount());
+
+        int maxElement = 0;
+        int minElement = array[0];
+        for(int i : array) {
+            if(i>maxElement) {
+                maxElement = i;
+            }
+        }
+
+        for(int i=0;i<array.length;i++) {
+            if(array[i]<minElement) {
+                minElement = array[i];
+            }
+        }
+        System.out.println("by iterating max element : "+maxElement);
+        System.out.println("by iterating min element : "+minElement);
+    }
+}
